13c58572a142738bf08a54a4df62a1265ccf6224
omer kor <omerkorr@gmail.com>
2020-02-07 00:30:55 +0300

    first

---
+++ istasyon/istasyon/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace istasyon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
